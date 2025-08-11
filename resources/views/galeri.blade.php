ccb6166186263ca6f7304b86b8831b8e699cfc65
hasni <hasniawati089@gmail.com>
2025-08-12 02:05:29 +0800

    tes

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -17,7 +17,7 @@
                                     <span class="visually-hidden">New</span>
                                 </span>
                             </span>
-                            <span class="btn-inner--text">Messages</span>
+                            <span class="btn-inner--text">Tambah</span>
                         </button>
                         <button type="button" class="btn btn-sm btn-dark btn-icon d-flex align-items-center mb-0">
                             <span class="btn-inner--icon">
@@ -27,7 +27,7 @@
                                         d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                                 </svg>
                             </span>
-                            <span class="btn-inner--text">Sync</span>
+                            <span class="btn-inner--text">Hapus</span>
                         </button>
                     </div>
                 </div>
